8c3d513afa4032b467372befa178a8415b3a17e4
temppase <temppase@gmail.com>
2021-06-01 10:13:30 +0300

    Add project files.

---
+++ TodoTest/Data/TodoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TodoTest.Data
+{
+    public class TodoModel
+    {
+        public string Table = "ToDoTable";
+        [BsonId]
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+    }
+}
